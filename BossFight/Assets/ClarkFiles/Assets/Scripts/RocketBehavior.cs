b035af0a790699fc29a5cf76de6187cd89e4a6ee
WheelchairBound64 <123192532+WheelchairBound64@users.noreply.github.com>
2024-12-12 00:33:58 -0600

    updated script

--- BossFight/Assets/ClarkFiles/Assets/Scripts/RocketBehavior.cs
+++ BossFight/Assets/ClarkFiles/Assets/Scripts/RocketBehavior.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class RocketBehavior : MonoBehaviour
+namespace ccomstock
 {
-    [SerializeField] Rigidbody rocketRB;
-    [SerializeField] float rocketSpeed;
-    [SerializeField] float rocketDespawnTime;
-    [SerializeField] AudioClip rocketFire;
-
-    private void Start()
+    public class RocketBehavior : MonoBehaviour
     {
-        rocketRB.AddForce(transform.forward * rocketSpeed * Time.deltaTime, ForceMode.Impulse);
-        StartCoroutine(DestroyRocket());
-        SoundEffectsManager.instance.PlayAudioClip(rocketFire, true);
-    }
+        [SerializeField] Rigidbody rocketRB;
+        [SerializeField] float rocketSpeed;
+        [SerializeField] float rocketDespawnTime;
+        [SerializeField] AudioClip rocketFire;
 
-    IEnumerator DestroyRocket()
-    {
-        yield return new WaitForSeconds(rocketDespawnTime);
-        Destroy(this.gameObject);
+        private void Start()
+        {
+            rocketRB.AddForce(transform.forward * rocketSpeed * Time.deltaTime, ForceMode.Impulse);
+            StartCoroutine(DestroyRocket());
+            SoundEffectsManager.instance.PlayAudioClip(rocketFire, true);
+        }
+
+        IEnumerator DestroyRocket()
+        {
+            yield return new WaitForSeconds(rocketDespawnTime);
+            Destroy(this.gameObject);
+        }
     }
 }
